817acd6ea97dd5987a90d9a1980eda5bc0905e19
ale-russ <agarrussom@gmail.com>
2020-11-12 13:43:30 +0300

    first commit on master

---
+++ src/components/Recipe.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+import RecipeDetails from "./RecipeDetails";
+
+const theme = {
+  root: {
+    backgroundColor: "#35c37d",
+    fontSize: "1.3rem",
+    borderRadius: 10,
+    left: 5,
+    "&:hover": {
+      backgroundColor: "white",
+      transition: "all .4s ease-in-out",
+    },
+  },
+};
+
+const Recipe = ({ recipe, classes }) => {
+  const [show, setShow] = useState(false);
+  const { label, image, url, ingredients } = recipe.recipe;
+
+  return (
+    <div className={classes.recipe}>
+      <h2>{label}</h2>
+      <img src={image} alt="label" />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        URL
+      </a>
+      <Button variant="contained" onClick={() => setShow(!show)}>
+        Ingredients
+      </Button>
+      {show && <RecipeDetails ingredients={ingredients} classes={classes} />}
+    </div>
+  );
+};
+
+export default Recipe;
